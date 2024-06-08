0645f6b11b25f6efa7ad3fa914d930fc95c6c807
NogaTal98 <isanyo.noga@gmail.com>
2024-06-08 20:32:27 +0300

    add game over message
    add a message about if the desired was found or not

--- frontend/src/components/ShowRace.js
+++ frontend/src/components/ShowRace.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { NUM_OF_ITERATIONS } from '../constants';
+
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -41,6 +43,18 @@ let options = {
   }
 }
 
+const renderSuccess = (chartData) => {
+  let lastData = chartData.datasets[0].data[chartData.datasets[0].data.length-1];
+  if (lastData !== undefined && lastData.toFixed(2) === (1).toFixed(2)) {
+    return <div className='goodResult'>Found the desired word within {chartData.labels.length - 1} tries!</div>
+  } else if (chartData.labels.length === NUM_OF_ITERATIONS + 1) {
+    return <div className='badResult'> Couldn't find the desired word within {NUM_OF_ITERATIONS} tries</div>;
+  }
+  else {
+    return null;
+    }
+}
+
 function ShowRace({ chartData, runAgain }) {
     return (
       <div className="show-page">
@@ -49,10 +63,12 @@ function ShowRace({ chartData, runAgain }) {
         <div className='wordList'> 
           {chartData.labels.map((label, index) => 
           <span>
-            <a href={"https://en.wikipedia.org/wiki/"+label} target="_blank">{label} </a>
+            <a href={"https://en.wikipedia.org/wiki/"+label} target="_blank" rel="noreferrer">{label} </a>
               {'('}{chartData.datasets[0].data[index].toFixed(2)}{') '}
             {index !== (chartData.labels.length - 1) && <FontAwesomeIcon icon={faArrowRight}/>} </span>)}
         </div>
+
+        {renderSuccess(chartData)}
 
         <div onClick={runAgain} className='start-btn'>Run again</div>
       </div>
